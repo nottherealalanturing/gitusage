c65780ff33ee844576c160c1f327bd00cc815c1b
Bin Eesaa <bin.eesaa@gmail.com>
2021-01-02 23:52:08 +0100

    add extra button for added functionality

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 document.getElementById('button-call').addEventListener('click', function(e){
     document.getElementById('test-para').style.color = blue;
 })
+
+document.getElementById('button-dial').addEventListener('click', function(e){
+    document.getElementById('test-tet').style.color = red;
+})
+
